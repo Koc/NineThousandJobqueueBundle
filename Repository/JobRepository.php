02b2750816692392cbc17b6812761019c2e698b9
Konstantin.Myakshin <koc-dp@yandex.ru>
2011-10-23 23:23:28 +0300

    refactored repository

--- Repository/JobRepository.php
+++ Repository/JobRepository.php
@@ -2,76 +2,63 @@
 
 namespace NineThousand\Bundle\NineThousandJobqueueBundle\Repository;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
-
-use Doctrine\DBAL\Types\Type as DoctrineType;
-use Doctrine\ORM\Query;
-use Doctrine\ORM\Query\Expr\Join;
-use Doctrine\ORM\NoResultException;
 
 class JobRepository extends EntityRepository
 {
-
     public function findAllByQuery($query = array())
     {
         $result = array();
-        $params = array();
-        if (!$query['reverse']) {
-            $order = 'DESC';
-        } else {
-            $order = 'ASC';
-        }
-        
-        $qb = $this->getEntityManager()->createQueryBuilder();
-        $qb->select('j');
-        $qb->from('NineThousandJobqueueBundle:Job', 'j');
-                    
-        //scheduled filter
+        $entitiesQb = $this->createQueryBuilder('j');
+
+        // scheduled filter
         if (!$query['scheduled']) {
-            $qb->andWhere('j.schedule is NULL');
+            $entitiesQb->andWhere('j.schedule is NULL');
         } else {
-            $qb->andWhere('j.schedule is not NULL');
+            $entitiesQb->andWhere('j.schedule is not NULL');
         }
-        
-        //status filter {
-        if (FALSE !== strpos($query['status'], 'f') ||
-           ($query['status'] === 0)) {
-            $qb->andWhere('j.status = :status');
-            $params['status'] = 'fail';
-        } else if (FALSE !== strpos($query['status'], 's') ||
-           ($query['status']===1)) {
-            $qb->andWhere('j.status = :status');
-            $params['status'] = 'success';
-        } else if (FALSE !== strpos($query['status'], 'r')) {
-            $qb->andWhere('j.retry = :retry');
-            $qb->andWhere('j.attempts < j.maxRetries');
-            $params['retry'] = 1;
-        } else if (FALSE !== strpos($query['status'], 'p')) {
-            $qb->andWhere('j.retry <> :retry');
-            $qb->andWhere('j.active = :active');
-            $params['retry'] = 1;
-            $params['active'] = 1;
-        }
-        
-        $qb->orderBy('j.lastrunDate ', $order)
-           ->setParameters($params);
 
-        $q = $qb->getQuery();
+        // status filter
+        if (false !== strpos($query['status'], 'f') || (0 === $query['status'])) {
+            $entitiesQb
+                ->andWhere('j.status = :status')
+                ->setParameter('status', 'fail');
+        } elseif (false !== strpos($query['status'], 's') || (1 === $query['status'])) {
+            $entitiesQb
+                ->andWhere('j.status = :status')
+                ->setParameter('status', 'success');
+        } elseif (false !== strpos($query['status'], 'r')) {
+            $entitiesQb
+                ->andWhere('j.retry = :retry')
+                ->andWhere('j.attempts < j.maxRetries')
+                ->setParameter('retry', 1);
+        } elseif (false !== strpos($query['status'], 'p')) {
+            $entitiesQb
+                ->andWhere('j.retry <> :retry')
+                ->andWhere('j.active = :active')
+                ->setParameter('retry', 1)
+                ->setParameter('active', 1);
+        }
+
         if (null !== $query['limit']) {
-            $countQuery = clone $q;
-            $countQuery->setParameters($q->getParameters());
+            $entitiesCountQb = clone $entitiesQb;
+            $entitiesCountQb->setParameters($entitiesQb->getParameters());
 
-            $q->setMaxResults($query['limit']);
-            if (null !== $query['offset']) {
-                $q->setFirstResult($query['offset']);
-            }
-            
-            $result['totalResults'] = count($countQuery->getResult()); 
+            $entitiesCountQb
+                ->resetDQLPart('select')
+                ->select('COUNT(j)');
+
+            $result['totalResults'] = $entitiesCountQb->getQuery()->getSingleScalarResult();
         }
-        
-        $result['entities'] = $q->getResult();
-        if (!$result['totalResults']) {
+
+        $entitiesQb
+            ->setMaxResults($query['limit'])
+            ->setFirstResult($query['offset'])
+            ->orderBy('j.lastrunDate ', $query['reverse'] ? 'ASC' : 'DESC');
+
+        $result['entities'] = $entitiesQb->getQuery()->getResult();
+
+        if (!isset($result['totalResults'])) {
             $result['totalResults'] = count($result['entities']);
         }
 
